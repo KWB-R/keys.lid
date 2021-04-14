92d180cc3b12a1bff709a3ea350ab89ca3e3e7eb
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-14 09:10:06 +0200

    Add fixes

--- R/.model_parameterisation.R
+++ R/.model_parameterisation.R
@@ -1,4 +1,4 @@
-mod <- swmmr::read_inp("sensitivity_analysis_models/model_greenroof_zone1.inp")
+mod <- swmmr::read_inp("inst/extdata/sensitivity/models/zone1/1.inp")
 mod$options
 
 mod$lid_usage
@@ -6,12 +6,12 @@ mod$lid_usage
 mod$lid_controls
 
 q_drain <- function(C, h, n = 0.5) {
-  
+
   C * h ^ n
 }
 
 C <- seq(2,150, 0.1)
-h <- 300 
+h <- 300
 
 C <- 1
 h <- seq(0,650,50)
@@ -20,8 +20,8 @@ plot(h, q_drain(C, h, n = 0.5))
 ### Abflussspendenwert (l/s/ha)
 
 # area_roof <- 65 #m2
-# area_rain.barrel <- 
-# rain_mm.per.hour <- 
+# area_rain.barrel <-
+# rain_mm.per.hour <-
 
 
 
@@ -33,11 +33,11 @@ suction.head_mm <- function(kf_mm.per.hour) {
   # n be derived from this table:
   # Ψ = 3.237K^-0.328  (R2 = 0.9)
 inch_to_mm <- 25.1
-  
-kf_inch.per.hour <- kf_mm.per.hour / inch_to_mm 
-      
+
+kf_inch.per.hour <- kf_mm.per.hour / inch_to_mm
+
 (3.237 * kf_inch.per.hour^-0.328) * inch_to_mm
-  
+
 }
 
 suction.head_mm (kf_mm.per.hour = 30)
