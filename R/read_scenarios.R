266e278c948ef2fd2bb9533efa8d4db369f45ec8
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-23 13:09:26 +0200

    fix read_scenarios()

--- R/read_scenarios.R
+++ R/read_scenarios.R
@@ -13,21 +13,22 @@
 read_scenarios <- function(
   scenarios_xlsx = extdata_file("scenarios/swmm_lid-parameterisation.xlsx")
 ) {
-sheets <- readxl::excel_sheets(scenarios_xlsx)
+lids <- readxl::excel_sheets(scenarios_xlsx)
 
 scenarios <- dplyr::bind_rows(stats::setNames(
-  lapply(sheets, function(sheet) {
-  readxl::read_xlsx(scenarios_xlsx, sheet = sheet)
+  lapply(lids, function(lid) {
+  lid <- readxl::read_xlsx(scenarios_xlsx, sheet = lid)
+  cols <- names(lid)[!names(lid) %in% c("lid_name_tidy",
+                                                    "type",
+                                                    "parameter_unit",
+                                                    "comment",
+                                                    "reference")]
+  tidyr::pivot_longer(lid,
+                      cols = cols,
+                      names_to = "scenario_name",
+                      values_to = "value")
 }), nm = sheets),
 .id = "lid_name_tidy")
 
-cols <- names(scenarios)[!names(scenarios) %in% c("lid_name_tidy",
-                                                  "type",
-                                                  "parameter_unit",
-                                                  "comment",
-                                                  "reference")]
-tidyr::pivot_longer(scenarios,
-                    cols = cols,
-                    names_to = "scenario_name",
-                    values_to = "value")
+
 }
