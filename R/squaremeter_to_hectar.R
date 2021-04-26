170d76f58bf55f81635cf86431766defe3dd82da
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-26 21:40:36 +0200

    Add squaremeter_to_hectar()

---
+++ R/squaremeter_to_hectar.R
@@ -0,0 +1,13 @@
+#' Helper function: squaremeter to hectr
+#'
+#' @param squaremeter squarmeter
+#'
+#' @return area in hectar
+#' @export
+#'
+#' @examples
+#' squaremeter_to_hectar(100)
+squaremeter_to_hectar <- function(squaremeter) {
+  squaremeter / 10000
+}
+
