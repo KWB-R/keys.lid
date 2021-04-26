6eaff372aaa2aa1e28beac6c91883b0d018db20d
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-26 21:46:26 +0200

    Start with permeable pavement scenario
    still unfinished

--- R/.run_scenarios.R
+++ R/.run_scenarios.R
@@ -1,7 +1,6 @@
-library(keys.lid)
+if(FALSE)  {
 
-lid_para <- readr::read_csv(kwb.swmm::extdata_file("lid/required_parameteristion.csv"))
-lid_para
+library(keys.lid)
 
 model_dir <- keys.lid::extdata_file("scenarios/models/zone1")
 
@@ -16,10 +15,93 @@ lid_scenarios <- keys.lid::read_scenarios()
 
 scenarios <- keys.lid::read_scenarios()
 
-scenarios %>%  dplyr::filter(lid_name_tidy == "permeable_pavement")
 
-lid <- "green_roof"
-scenario_name <
+lid <- "permeable_pavement"
 
-scenarios[scenarios$lid_name_tidy == lid,]
+lid_selected <- scenarios %>%  dplyr::filter(.data$lid_name_tidy == lid)
+
+scenario_names <- unique(lid_selected$scenario_name)
+
+selected_scenario <- scenario_names[1]
+
+lid_selected_scenario <- lid_selected %>%  dplyr::filter(.data$scenario_name == selected_scenario)
+
+lid_controls <- lidconfig_to_swmm(lid_selected_scenario)
+
+
+
+subcatchment <- tibble::tibble(Name = "S1",
+                               `Rain Gage` = "RainGage",
+                               Outlet = "Out1",
+                               Area = keys.lid::squaremeter_to_hectar(1000),
+                               Perc_Imperv = 100,
+                               Width = 6,
+                               Perc_Slope = 0.5,
+                               Curb_Len = 0,
+                               Snowpack = "snowPack1"
+                               )
+
+swmm_inp$subcatchments <- subcatchment
+
+
+lid_usage <- tibble::tibble("Subcatchment" = "S1",
+                    "LID Process" = lid_controls$Name[1],
+                    "Number" = 1,
+                    "Area" = 65,
+                    "Width" = 6,
+                    "InitSat" = 0,
+                    "FromImp" = 0,
+                    "ToPerv" = "*",
+                    "DrainTo" = "*"
+                    )
+
+path_inp_file <- paste0(sprintf("%s/%s",
+                                model_dir,
+                                stringr::str_replace_all(lid_controls$Name[1],
+                                                     pattern = "\\.",
+                                                     replacement = "__")),
+                        ".inp")
+swmm_inp$lid_controls <- lid_controls
+swmm_inp$lid_usage  <- lid_controls$Name[1] #lid_usage
+
+#Temperature file
+swmm_inp$temperature$Values[swmm_inp$temperature$`Data Element` == "FILE"]
+
+#Rain file
+swmm_inp$raingages$Source
+
+
+swmmr::write_inp(swmm_inp, file = path_inp_file)
+swmmr::run_swmm(path_inp_file)
+
+}
+
+lidconfig_to_swmm <- function(df) {
+
+  lid_para <- readr::read_csv(kwb.swmm::extdata_file("lid/required_parameteristion.csv"))
+
+  lid_parametersation <- df %>%
+    dplyr::select(tidyselect::all_of(c("lid_name_tidy", "type", "id_type_parameter", "scenario_name", "value"))) %>%
+    dplyr::left_join(lid_para  %>%
+                       dplyr::select(.data$lid_id, .data$lid_name_tidy), by = "lid_name_tidy") %>%
+    dplyr::mutate("Name" = sprintf("%s.%s", .data$lid_name_tidy, .data$scenario_name),
+                  "Type/Layer" =  stringr::str_to_upper(.data$type),
+                  ) %>%
+    dplyr::select(tidyselect::all_of(c("Name", "Type/Layer", "id_type_parameter", "value"))) %>%
+    tidyr::pivot_wider(names_from = "id_type_parameter",
+                       names_prefix = "Par",
+                       values_from = "value")
+
+  lid_parametersation
+
+  lid_id <- lid_para$lid_id[lid_para$lid_name_tidy == unique(df$lid_name_tidy)]
+
+  lid_header <-   lid_parametersation[1,]
+  lid_header[1,3:ncol(lid_header)] <- NA_real_
+  lid_header$`Type/Layer` <- lid_id
+
+  dplyr::bind_rows(lid_header, lid_parametersation)
+
+}
+
 
